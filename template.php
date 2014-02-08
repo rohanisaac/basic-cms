ad7cb7d08f3ff520e3fa22e029a3c5304ec7e179
Rohan Isaac <rohan_isaac@yahoo.com>
2014-02-08 12:40:30 -0500

    Template page with some includes

---
+++ template.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Page Title</title>
+	<meta name="description" content="Description here">
+	<meta name="keywords" content="insert keywords here">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	<?php include('includes/header.php') ?>
+	<?php include('includes/navigation.php') ?>
+	<?php include('includes/content.php') ?>
+	<?php include('includes/footer.php') ?>
+</body>
+</html>
