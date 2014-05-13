da1aa2d3b4ade7997cef55571fa66f057fa1fbf9
Rohan Isaac <rohan_isaac@yahoo.com>
2014-05-13 19:43:18 -0400

    Minor changes

--- welcome.php
+++ welcome.php
@@ -8,18 +8,9 @@ $body = '
 
 <h1>Welcome</h1>
 
-<p>
+<p>This is a welcome page. It should welcome you to the site.</p>
 
-	This is a welcome page. It should welcome you to the site.
-
-</p>
-
-<p>
-
-	<a href="blog.php">This link</a> should take you to another kind of page, with a different template. There should be no header
-	and footer on this page, but on the next there will be.
-
-</p>
+<p><a href="blog.php">This blog link</a> should take you to another kind of page, with a different template. There should be no header and footer on this page, but on the next there will be.</p>
 
 ';
 
