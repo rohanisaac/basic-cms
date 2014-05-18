fc046c7cd18f01a64ede666d5302b454cad23707
Rohan Isaac <rohan_isaac@yahoo.com>
2014-05-17 21:57:07 -0400

    Split metadata and data

--- sitepages/welcome.php
+++ sitepages/welcome.php
@@ -1,19 +1,11 @@
 <!-- Sample page that loads some data, and sends it to the template -->
 <?php
 $filename = __FILE__;
-$id = "welcome"; // if this is going to work as a database, we will need a unique key
+$id = "welcome"; // unique key, body file name
 $title = "Welcome";
 $description = "Page to welcome readers";
 $keywords = "sample,page,website,other,stupid,keywords";
-$body = '
-
-<p>This is a welcome page. It should welcome you to the site.</p>
-
-<p><a href="blog.php">This blog link</a> should take you to another kind of page, with a different template. There should be no header and footer on this page, but on the next there will be.</p>
-
-<p> Lots of things need to work better. </p>
-
-';
+$body = file_get_contents($id.'.txt');
 
 $parent = "none"; // top level page if none, otherwise, page title
 
