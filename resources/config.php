d12f4a36f83e8c6a838544583e0d2c0ec18eb018
Rohan Isaac <rohan_isaac@yahoo.com>
2014-05-17 22:34:59 -0400

    made php page

--- resources/config.php
+++ resources/config.php
@@ -1,5 +1,9 @@
+<?php
+
 /* this is where all our settings (like global directory variables) and function files could be pulled settings. I did this with my site, and it seems to work pretty well, though it does necessarily add a little bloat for some pagest that don't need everything (like most of them), but it does cut down on complexity something to think about */
 
 /* Looks good, let's do it - R*/
 
 $SITE_NAME = 'Basic CMS II';
+
+?>
