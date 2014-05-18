d4d1274b19a334a7fd0021eae7aca5fbfcf579d4
Rohan Isaac <rohan_isaac@yahoo.com>
2014-05-17 22:39:19 -0400

    Moved more to markdown

--- sitepages/index.php
+++ sitepages/index.php
@@ -1,2 +1,16 @@
-<META HTTP-EQUIV="Refresh" CONTENT="0; URL=welcome.php">
-	<!-- Should change to php or better redirect-->
+<!-- Sample page that loads some data, and sends it to the template -->
+<?php
+$filename = __FILE__;
+$id = "welcome"; // unique key, body file name
+$title = "Welcome";
+$description = "Page to welcome readers";
+$keywords = "sample,page,website,other,stupid,keywords";
+$body = file_get_contents($id.'.txt');
+
+$parent = "none"; // top level page if none, otherwise, page title
+
+define("THISISTHEKEY", "welcome");
+
+include('../resources/templates/_template.php');
+
+?>
--- sitepages/sample.php
+++ sitepages/sample.php
@@ -5,7 +5,7 @@ $id = "123"; // if this is going to work as a database, we will need a unique ke
 $title = "Sample Page";
 $description = "This is a good description";
 $keywords = "sample,page,website,other,stupid,keywords";
-$body = "This is the body. I don't like this style of encapsulations. It would be better if we could embed straigt html into here."	;
+$body = file_get_contents($id.'.txt');
 $parent = "none"; // top level page if none, otherwise, page title
 
 define("THISISTHEKEY", "This is the key");
